cfc5b48d03495ac90c3e204169e7dcd4c4b97a63
Arun Tyagi <arun.tyagi@salesforce.com>
2025-12-24 12:37:00 +0530

    fix vulnerabilities and core update

--- test/setup-tests.ts
+++ test/setup-tests.ts
@@ -6,3 +6,9 @@
 // To suppress these warnings, it is easiest to just allow the process running these tests to allow an infinite amount
 // of listeners. So we do that here (where 0 means infinite):
 process.setMaxListeners(0);
+
+// Some dependencies (Ink via @oclif/table) patch the console and expect `console.Console` to exist.
+// In the Vitest environment, the global console can be a proxied object without a `Console` constructor.
+// Ensure `console.Console` points to Node's Console to avoid "console.Console is not a constructor" errors.
+import { Console as NodeConsole } from 'node:console';
+(globalThis.console as unknown as { Console?: typeof NodeConsole }).Console ??= NodeConsole;
